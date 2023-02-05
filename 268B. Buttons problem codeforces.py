6057186289f428575bb449426e3bb583cc35fea7
wiwocodes <109910553+wiwocodes@users.noreply.github.com>
2023-02-05 14:02:32 +0700

    Add files via upload

---
+++ 268B. Buttons problem codeforces.py
@@ -0,0 +1,18 @@
+number = int(input())
+n = 1
+equation = number - n
+equa = n * equation
+variables = []
+for x in range(number):
+    equation = number - n
+    equa = n * equation
+    equa = equa + 1
+    variables.append(equa)
+    n = n + 1
+    
+
+add = sum(variables)
+print(add)
+    
+
+#maybe make code where you make  functions to find each number how many pushes u need for example 7 make 7 functions named 1-7 and make a funtion in each of em to find shit or use a for loop idk
